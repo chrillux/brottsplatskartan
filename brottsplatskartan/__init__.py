82a26e873761dabaf1296618f471b5953a235962
G Johansson <goran.johansson@shiftit.se>
2023-12-17 21:25:11 +0100

    Typing and format

--- brottsplatskartan/__init__.py
+++ brottsplatskartan/__init__.py
@@ -4,17 +4,32 @@
 import datetime
 import time
 from json.decoder import JSONDecodeError
-from typing import Union
+from typing import Dict, Literal, Union, List
 
 import requests
 
 AREAS = [
-    "Blekinge län", "Dalarnas län", "Gotlands län", "Gävleborgs län",
-    "Hallands län", "Jämtlands län", "Jönköpings län", "Kalmar län",
-    "Kronobergs län", "Norrbottens län", "Skåne län", "Stockholms län",
-    "Södermanlands län", "Uppsala län", "Värmlands län", "Västerbottens län",
-    "Västernorrlands län", "Västmanlands län", "Västra Götalands län",
-    "Örebro län", "Östergötlands län"
+    "Blekinge län",
+    "Dalarnas län",
+    "Gotlands län",
+    "Gävleborgs län",
+    "Hallands län",
+    "Jämtlands län",
+    "Jönköpings län",
+    "Kalmar län",
+    "Kronobergs län",
+    "Norrbottens län",
+    "Skåne län",
+    "Stockholms län",
+    "Södermanlands län",
+    "Uppsala län",
+    "Värmlands län",
+    "Västerbottens län",
+    "Västernorrlands län",
+    "Västmanlands län",
+    "Västra Götalands län",
+    "Örebro län",
+    "Östergötlands län",
 ]
 
 ATTRIBUTION = "Information provided by brottsplatskartan.se"
@@ -22,10 +37,16 @@ BROTTS_URL = "https://brottsplatskartan.se/api"
 
 
 class BrottsplatsKartan:  # pylint: disable=too-few-public-methods
-    """ Brottsplatskartan API wrapper. """
+    """Brottsplatskartan API wrapper."""
 
-    def __init__(self, app='bpk', areas=None, longitude=None, latitude=None):
-        """ Setup initial brottsplatskartan configuration. """
+    def __init__(
+        self,
+        app: str = "bpk",
+        areas: List[AREAS] = None,
+        longitude: Union[float, None] = None,
+        latitude: Union[float, None] = None,
+    ):
+        """Setup initial brottsplatskartan configuration."""
 
         self.parameters = {"app": app}
         self.incidents = {}
@@ -33,7 +54,7 @@ class BrottsplatsKartan:  # pylint: disable=too-few-public-methods
         if areas:
             for area in areas:
                 if area not in AREAS:
-                    raise ValueError('not a valid area: {}'.format(area))
+                    raise ValueError("not a valid area: {}".format(area))
             self.url = BROTTS_URL + "/events"
             self.parameters["areas"] = areas
         elif longitude and latitude:
@@ -47,32 +68,33 @@ class BrottsplatsKartan:  # pylint: disable=too-few-public-methods
 
     @staticmethod
     def _get_datetime_as_ymd(date: time.struct_time) -> datetime.datetime:
-
-        datetime_ymd = datetime.datetime(date.tm_year, date.tm_mon,
-                                         date.tm_mday)
+        datetime_ymd = datetime.datetime(date.tm_year, date.tm_mon, date.tm_mday)
 
         return datetime_ymd
 
     @staticmethod
     def is_ratelimited(requests_response) -> bool:
-        """ Check if we have been ratelimited. """
-        rate_limited = requests_response.headers.get('x-ratelimit-reset')
+        """Check if we have been ratelimited."""
+        rate_limited = requests_response.headers.get("x-ratelimit-reset")
         if rate_limited:
-            print("You have been rate limited until " + time.strftime(
-                '%Y-%m-%d %H:%M:%S%z', time.localtime(int(rate_limited))))
+            print(
+                "You have been rate limited until "
+                + time.strftime(
+                    "%Y-%m-%d %H:%M:%S%z", time.localtime(int(rate_limited))
+                )
+            )
             return True
         return False
 
     def get_incidents_from_bpk(self, parameters) -> Union[list, bool]:
-        """ Make the API calls to get incidents """
+        """Make the API calls to get incidents"""
 
         brotts_entries_left = True
         incidents_today = []
         url = self.url
 
         while brotts_entries_left:
-
-            requests_response = requests.get(url, params=parameters)
+            requests_response = requests.get(url, params=parameters,timeout=8)
 
             if self.is_ratelimited(requests_response):
                 return False
@@ -89,14 +111,12 @@ class BrottsplatsKartan:  # pylint: disable=too-few-public-methods
                 break
 
             datetime_today = datetime.date.today()
-            datetime_today_as_time = time.strptime(str(datetime_today),
-                                                   "%Y-%m-%d")
+            datetime_today_as_time = time.strptime(str(datetime_today), "%Y-%m-%d")
             today_date_ymd = self._get_datetime_as_ymd(datetime_today_as_time)
 
             for incident in incidents:
                 incident_pubdate = incident["pubdate_iso8601"]
-                incident_date = time.strptime(incident_pubdate,
-                                              "%Y-%m-%dT%H:%M:%S%z")
+                incident_date = time.strptime(incident_pubdate, "%Y-%m-%dT%H:%M:%S%z")
                 incident_date_ymd = self._get_datetime_as_ymd(incident_date)
 
                 if today_date_ymd == incident_date_ymd:
@@ -112,8 +132,8 @@ class BrottsplatsKartan:  # pylint: disable=too-few-public-methods
 
         return incidents_today
 
-    def get_incidents(self) -> Union[list, bool]:
-        """ Get today's incidents. """
+    def get_incidents(self) -> Union[Dict, bool]:
+        """Get today's incidents."""
         areas = self.parameters.get("areas")
         all_incidents = {}
         current_incidents = []
